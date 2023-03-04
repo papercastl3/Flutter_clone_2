305389b7c64773cf53a3b2577944ec06418d5dfa
papercastl3 <jisung061444@gmail.com>
2023-03-04 11:03:01 +0900

    mainAxisAlignment change

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -77,6 +77,7 @@ class _HomeScreenState extends State<HomeScreen> {
           flex: 3,
           child: Center(
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 IconButton(
                   iconSize: 120,
